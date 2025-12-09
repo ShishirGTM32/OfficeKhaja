064d93cbcc3abbda163009bb21b3fd24e8115cbd
ShishirGTM32 <shishirgautam0223@gmail.com>
2025-12-09 16:42:42 +0545

    Setup of the smtp mail protocol

--- officekhaja/settings.py
+++ officekhaja/settings.py
@@ -179,3 +179,12 @@ SIMPLE_JWT = {
     
     'JTI_CLAIM': 'jti',
 }
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+DEFAULT_FROM_EMAIL = os.getenv('EMAIL_HOST_USER')
