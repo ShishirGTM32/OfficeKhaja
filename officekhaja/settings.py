cff06e6284b65b3243b3ace5c275fa484e2c72e7
ShishirGTM32 <shishirgautam0223@gmail.com>
2025-12-22 17:49:19 +0545

    Updating with swagger api

--- officekhaja/settings.py
+++ officekhaja/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 INSTALLED_APPS = [
@@ -43,6 +43,8 @@ INSTALLED_APPS = [
     'rest_framework.authtoken',
     'rest_framework_simplejwt',
     'rest_framework_simplejwt.token_blacklist',
+    'drf_spectacular',
+    'drf_yasg',
     'corsheaders',
     'khaja',
     'orders',
@@ -137,6 +139,7 @@ USE_I18N = True
 USE_TZ = True
 
 REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
@@ -216,6 +219,13 @@ CACHES = {
             "CLIENT_CLASS": "django_redis.client.DefaultClient",
         }
     }
+}
+
+SPECTACULAR_SETTINGS = {
+    'TITLE': 'My API',
+    'DESCRIPTION': 'API documentation for frontend integration',
+    'VERSION': '1.0.0',
+    'SERVE_INCLUDE_SCHEMA': False,
 }
 
 
--- officekhaja/urls.py
+++ officekhaja/urls.py
@@ -1,26 +1,28 @@
 """
-URL configuration for officekhaja project.
+URL configuration for officekhaja project using DRF Spectacular.
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/6.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+This setup includes:
+- Versioned API URLs for frontend integration
+- Swagger UI and Redoc for API documentation
+- Nested/related fields support in serializers
 """
+
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+# DRF Spectacular
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularSwaggerView,
+    SpectacularRedocView
+)
+
 urlpatterns = [
+
     path('admin/', admin.site.urls),
+
     path('api/khaja/', include('khaja.urls')),
     path('api/orders/', include('orders.urls')),
     path('api/auth/', include('users.urls')),
@@ -29,6 +31,10 @@ urlpatterns = [
     path('api/blogs/', include('blog.urls')),
     path('api/noti/', include('notifications.urls')),
     path('api/chat/', include('chatapp.urls')),
+
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'), 
+    path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 ]
 
 if settings.DEBUG:
